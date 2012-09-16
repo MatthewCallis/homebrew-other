b45c884cede99a323beb4dd4d8ac6ff4553d3e25
Matthew Callis <matthew.callis@gmail.com>
2012-09-15 18:11:58 -0700

    Test out bass install.

--- byuu/bass.rb
+++ byuu/bass.rb
@@ -7,8 +7,29 @@ class Bass < Formula
 
   depends_on 'gcc' # 4.7+
 
+  # Point to the correct GCC, not MacPorts.
+  def patches
+    DATA
+  end
+
   def install
     # system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make"
+    system "make install"
   end
 end
+
+__END__
+diff --git a/nall/Makefile b/nall/Makefile
+index bbc4b02..a9913c8 100755
+--- a/byuu/Makefile
++++ b/byuu/Makefile
+@@ -38,7 +38,7 @@ ifeq ($(compiler),)
+   ifeq ($(platform),win)
+     compiler := gcc
+   else ifeq ($(platform),osx)
+-    compiler := gcc-mp-4.7
++    compiler := gcc-4.7
+   else
+     compiler := gcc-4.7
+   endif
